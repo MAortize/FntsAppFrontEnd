3cbd27e327debbf575be054f137ba63f37b992f5
MAortize <ortiz.eche@gmail.com>
2023-09-08 14:10:07 -0500

    Se hacen mas media querys, se acomodan paleta de colores y se inicia el esqueleto de la pantalla de aprendizaje

--- src/app/learn/components/learn-view/learn-view.component.ts
+++ src/app/learn/components/learn-view/learn-view.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class LearnViewComponent {
 
+  disabled = true
+
+
+  constructor(){  }
+
+
+
 }
--- src/app/profile/components/profile-view/profile-view.component.ts
+++ src/app/profile/components/profile-view/profile-view.component.ts
@@ -7,5 +7,25 @@ import { Component } from '@angular/core';
 })
 export class ProfileViewComponent {
   email: string = sessionStorage.getItem('email')!
+  nickName: string = sessionStorage.getItem('username')!
+  urlPicProfile: string = sessionStorage.getItem('urlPic')!
+
+  changeData: boolean = true
+
+  value: string = "Modificar datos"
+
+  constructor(){
+  }
+
+
+  activarCambioData(){
+    this.changeData = !this.changeData
+    if (this.changeData==false) {
+      this.value = "Modificando datos"
+    }else{
+      this.value = "Modificar  datos"
+    }
+  }
+
 
 }
